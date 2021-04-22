ce78532d024f3c704f9b60117b0a91e48844f350
Jade <software@lfcode.ca>
2021-04-22 01:20:25 -0700

    add MkdirAction

--- polkadots/polkadots.py
+++ polkadots/polkadots.py
@@ -14,7 +14,7 @@ import runpy
 from . import version
 
 
-CONFIG_DIRECTORY = Path('~/.config/polkadots').expanduser()
+CONFIG_DIRECTORY = Path("~/.config/polkadots").expanduser()
 
 
 class Error(Exception):
@@ -30,7 +30,6 @@ class NotALink(Error):
 
 
 class Action(metaclass=abc.ABCMeta):
-
     def __init__(self, dotfile_repo, **args):
         """
         Instantiate an Action.
@@ -55,8 +54,15 @@ class CopyAction(Action):
     An action that copies files and optionally overwrites them
     """
 
-    def __init__(self, dotfile_repo, source, destination, dir_mode=False,
-                 overwrite=False, **kwargs):
+    def __init__(
+        self,
+        dotfile_repo,
+        source,
+        destination,
+        dir_mode=False,
+        overwrite=False,
+        **kwargs,
+    ):
         """
         Create a CopyAction
 
@@ -71,8 +77,7 @@ class CopyAction(Action):
                     names in the destination directory?
         """
         self.source = get_intuitive_path(source, base=dotfile_repo)
-        self.destination = get_intuitive_path(destination,
-                                              base=dotfile_repo)
+        self.destination = get_intuitive_path(destination, base=dotfile_repo)
         self.dotfile_repo = dotfile_repo
         self.overwrite = overwrite
         self.dir_mode = dir_mode
@@ -80,31 +85,54 @@ class CopyAction(Action):
     def execute(self):
         files = []
         if self.dir_mode:
-            files.extend((os.path.join(self.source, f)
-                          for f in os.listdir(self.source)))
+            files.extend(
+                (os.path.join(self.source, f) for f in os.listdir(self.source))
+            )
         else:
             files.append(self.source)
         for f in files:
             if os.path.isdir(self.destination):
-                if os.path.exists(os.path.join(self.destination,
-                                  os.path.basename(f))) and not \
-                                  self.overwrite:
-                    logging.warning('Skipping file {} because overwrite '
-                                    'is False and it exists in the '
-                                    'destination'.format(f))
+                if (
+                    os.path.exists(os.path.join(self.destination, os.path.basename(f)))
+                    and not self.overwrite
+                ):
+                    logging.warning(
+                        "Skipping file {} because overwrite "
+                        "is False and it exists in the "
+                        "destination".format(f)
+                    )
                     continue
             else:
                 if os.path.exists(self.destination) and not self.overwrite:
-                    logging.warning('Skipping file {} because overwrite is '
-                                    'False'.format(f))
+                    logging.warning(
+                        "Skipping file {} because overwrite is " "False".format(f)
+                    )
                     continue
             shutil.copy(f, self.destination)
 
 
-class SymlinkAction(Action):
+class MkdirAction(Action):
+    def __init__(self, dotfile_repo, directory, parents=True):
+        """
+        Creates the specified directory if it doesn't exist already.
 
-    def __init__(self, dotfile_repo, source, destination, dir_mode=False,
-                 **args):
+        Positional arguments:
+        dotfile_repo -- used as the base for `directory`
+        directory -- the directory to create
+
+        Keyword arguments:
+        parents -- should we create any nonexistent parent directories?
+        """
+        self.directory = get_intuitive_path(directory, base=dotfile_repo)
+        self.parents = parents
+
+    def execute(self):
+        logging.info("Mkdir %r", self.directory)
+        Path(self.directory).mkdir(parents=self.parents, exist_ok=True)
+
+
+class SymlinkAction(Action):
+    def __init__(self, dotfile_repo, source, destination, dir_mode=False, **args):
         """
         Instantiate a SymlinkAction
 
@@ -119,8 +147,7 @@ class SymlinkAction(Action):
                     names in the destination directory?
         """
         self.source = get_intuitive_path(source, base=dotfile_repo)
-        self.destination = get_intuitive_path(destination,
-                                              base=dotfile_repo)
+        self.destination = get_intuitive_path(destination, base=dotfile_repo)
         self.dir_mode = dir_mode
 
     def execute(self):
@@ -130,11 +157,11 @@ class SymlinkAction(Action):
                 fsource = os.path.join(self.source, f)
                 rmlink(fdest, ignore_absent=True)
                 os.symlink(fsource, fdest)
-                logging.info('Linked %s to %s', fsource, fdest)
+                logging.info("Linked %s to %s", fsource, fdest)
         else:
             rmlink(self.destination, ignore_absent=True)
             os.symlink(self.source, self.destination)
-            logging.info('Linked %s to %s', self.source, self.destination)
+            logging.info("Linked %s to %s", self.source, self.destination)
 
 
 class CatAction(Action):
@@ -150,38 +177,46 @@ class CatAction(Action):
         *sources     -- any number of source files that will be in the provided
                         order
         """
-        self.sources = [get_intuitive_path(source, base=dotfile_repo)
-                        for source in sources]
+        self.sources = [
+            get_intuitive_path(source, base=dotfile_repo) for source in sources
+        ]
         self.destination = get_intuitive_path(destination, base=dotfile_repo)
 
     def execute(self):
-        with open(self.destination, 'wb') as out:
+        with open(self.destination, "wb") as out:
             for src in self.sources:
-                with open(src, 'rb') as h:
+                with open(src, "rb") as h:
                     out.write(h.read())
 
 
 def rmlink(link, ignore_absent=False):
     if os.path.islink(link):
-        logging.info('Deleting {}'.format(link))
+        logging.info("Deleting link {}".format(link))
         os.remove(link)
     elif not os.path.exists(link):
-        logging.debug('File missing when trying to delete {}'.format(link))
+        logging.debug("File missing when trying to delete {}".format(link))
         if not ignore_absent:
-            raise FileNotFoundError('{link} when attempting to delete it as '
-                                    'a link'.format(link))
+            raise FileNotFoundError(
+                "{link} when attempting to delete it as a link".format(link)
+            )
     else:
-        raise NotALink(f'Found that {link} is not a link when attempting '
-                        'overwrite. Please fix your config or '
-                        'move this file')
+        raise NotALink(
+            f"Found that {link} is not a link when attempting "
+            "overwrite. Please fix your config or "
+            "move this file"
+        )
 
 
-def get_intuitive_path(path, base='.'):
+def get_intuitive_path(path, base="."):
     """
     Take a path and get the absolute, user expanded, variable expanded version
     """
+    # XXX: this is cursed
+    cwd = os.getcwd()
     os.chdir(os.path.abspath(os.path.expanduser(os.path.expandvars(base))))
-    return os.path.abspath(os.path.expanduser(os.path.expandvars(path)))
+    ret = os.path.abspath(os.path.expanduser(os.path.expandvars(path)))
+    os.chdir(cwd)
+    return ret
 
 
 def load_conf(f):
@@ -205,22 +240,24 @@ def get_actions(action_list, dotfile_repo):
     actions = []
     for action in action_list:
         try:
-            logging.info('Processing {}'.format(action['type']))
-            klass = getattr(sys.modules[__name__], action['type'])
+            logging.info("Processing {}".format(action["type"]))
+            klass = getattr(sys.modules[__name__], action["type"])
             actions.append(klass(dotfile_repo=dotfile_repo, **action))
         except:
-            logging.error('Error creating Action {}. It might not be '
-                          'implemented'.format(action['type']))
+            logging.error(
+                "Error creating Action {}. It might not be "
+                "implemented".format(action["type"])
+            )
             raise
     return actions
 
 
 def get_config(config: str):
     if os.path.exists(config) and os.path.isfile(config):
-        logging.info('Loading config from {}'.format(config))
+        logging.info("Loading config from {}".format(config))
         return load_conf(config)
     else:
-        logging.error('No config found in {}'.format(config))
+        logging.error("No config found in {}".format(config))
         sys.exit(1)
 
 
@@ -243,23 +280,24 @@ def load_conf2(config_dir: Path) -> Dict:
     Returns: a dict with the unserialized config
     """
     if not config_dir.is_dir():
-        raise ValueError(f'Provided config directory {config_dir} is not '
-                          'a directory')
-    with (config_dir / 'dotfile_repo').open() as h:
+        raise ValueError(
+            f"Provided config directory {config_dir} is not " "a directory"
+        )
+    with (config_dir / "dotfile_repo").open() as h:
         dotfile_repo = Path(h.read().rstrip()).expanduser()
 
-    f = config_dir / 'config.py'
+    f = config_dir / "config.py"
     actions = Action.__subclasses__()
-    conf_globals = {a.__name__:
-                    functools.partial(a, dotfile_repo)
-                    for a in actions}
+    conf_globals = {a.__name__: functools.partial(a, dotfile_repo) for a in actions}
     conf = runpy.run_path(f, init_globals=conf_globals)
     return conf
 
 
-def get_config_path(basedir: Path = CONFIG_DIRECTORY,
-                    profile_name: Optional[str] = None,
-                    config2: bool = False) -> str:
+def get_config_path(
+    basedir: Path = CONFIG_DIRECTORY,
+    profile_name: Optional[str] = None,
+    config2: bool = False,
+) -> str:
     """
     Find the correct path to pull a config from
 
@@ -270,23 +308,34 @@ def get_config_path(basedir: Path = CONFIG_DIRECTORY,
     """
     path = basedir
     if profile_name:
-        path = path / 'profiles' / profile_name
+        path = path / "profiles" / profile_name
     if config2:
         return path
     else:
-        return path / 'config.json'
+        return path / "config.json"
 
 
 def main():
-    ap = argparse.ArgumentParser(description='Yet another dotfile manager')
-    ap.add_argument('--verbose', '-v', action='count', default=0)
-    ap.add_argument('--config2', '-2', action='store_true',
-                    help='Use new python-based config format')
-    ap.add_argument('--config', '-c', help='Config to use rather than the '
-                    'default. Can be a directory')
-    ap.add_argument('--profile', '-p', help='Like --config, except it loads'
-                    'a profile from the polkadots/profiles directory')
-    ap.add_argument('--version', action='store_true')
+    ap = argparse.ArgumentParser(description="Yet another dotfile manager")
+    ap.add_argument("--verbose", "-v", action="count", default=0)
+    ap.add_argument(
+        "--config2",
+        "-2",
+        action="store_true",
+        help="Use new python-based config format",
+    )
+    ap.add_argument(
+        "--config",
+        "-c",
+        help="Config to use rather than the default. Can be a directory",
+    )
+    ap.add_argument(
+        "--profile",
+        "-p",
+        help="Like --config, except it loads "
+        "a profile from the polkadots/profiles directory",
+    )
+    ap.add_argument("--version", action="store_true")
     args = ap.parse_args()
 
     if args.version:
@@ -302,20 +351,17 @@ def main():
 
     logging.basicConfig(level=log_level)
 
-    config_path = get_config_path(
-        profile_name=args.profile,
-        config2=args.config2
-    )
+    config_path = get_config_path(profile_name=args.profile, config2=args.config2)
     if args.config2:
         conf = load_conf2(config_path)
-        actions = conf['actions']
+        actions = conf["actions"]
     else:
         conf = get_config(config_path)
-        actions = get_actions(conf['actions'], conf['dotfile_repo'])
+        actions = get_actions(conf["actions"], conf["dotfile_repo"])
 
     for action in actions:
         action.execute()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
